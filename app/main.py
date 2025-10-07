84746c6fa5b94774e6f5a4b89fbab912136cad84
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2025-10-07 15:48:52 +0300

    bug-fix: Repo fix...

--- app/main.py
+++ app/main.py
@@ -6,13 +6,16 @@ Initializes database, services, and UI components.
 
 from app.db.init_sqlite import init_sqlite
 from app.config import provide_app_config
+from app.repo import init_repos
 
 def main():
     """Start the application."""
     # Load config & DB
     config = provide_app_config()
     connection = init_sqlite(config.db_settings)
-    print(connection)
+    repos = init_repos(connection)
+    print(f" Repo: {repos}")
+    print("App started.")
 
 
 if __name__ == "__main__":
--- app/services/task_service.py
+++
@@ -1,9 +0,0 @@
-# app/services/task_service.py
-
-"""
-TaskService
------------
-Provides business logic operations for the Task model.
-Handles creation, updates, deletion, retrieval, filtering,
-and other task-specific operations through the Repo layer.
-"""
